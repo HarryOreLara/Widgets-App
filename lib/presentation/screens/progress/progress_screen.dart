7af4313b29395623d1a8554f5a32832a447b6eda
HarryOreLara <harryorelara@gmail.com>
2023-08-02 22:33:42 -0500

    Agregango nuevo widgets

--- lib/presentation/screens/progress/progress_screen.dart
+++ lib/presentation/screens/progress/progress_screen.dart
@@ -1,10 +1,48 @@
 import 'package:flutter/material.dart';
 
 class ProgressScreen extends StatelessWidget {
-
-  static const name = 'progress_screen';//cada pantalla con su nombre
+  static const name = 'progress_screen'; //cada pantalla con su nombre
 
   const ProgressScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Progress Indicators'),
+      ),
+      body: const _ProgressView(),
+    );
+  }
+}
+
+
+
+class _ProgressView extends StatelessWidget {
+  const _ProgressView();
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Column(
+        children: [
+          SizedBox(height: 30,),
+          Text('Cicular progress indicator'),
+          SizedBox(height: 10,),
+          CircularProgressIndicator(strokeWidth: 2, backgroundColor: Colors.black45,),
+
+          SizedBox(height: 20,),
+          Text('Circular indicator controlado'),
+          SizedBox(height: 10,)
+        ],
+      ),
+    );
+  }
+}
+
+
+class _ControllerProgressIndicator extends StatelessWidget {
+  const _ControllerProgressIndicator ();
 
   @override
   Widget build(BuildContext context) {
